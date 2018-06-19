07b4b2b3ad251f2f4db3af7cd2d31c62647cae90
DinukaX4 <31525330+DinukaX4@users.noreply.github.com>
2018-06-19 23:00:25 +0530

    client-profile-part-III

--- FrontEnd/Client/src/app/client-profile/client-profile.component.ts
+++ FrontEnd/Client/src/app/client-profile/client-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-client-profile',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-profile.component.css']
 })
 export class ClientProfileComponent implements OnInit {
+  url: any = 'assets/img/faces/avatar.jpg';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  readUrl(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      }
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
 }
